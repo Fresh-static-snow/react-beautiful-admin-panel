7d84f15fe4707c64d0e31d250697c745fd3bb901
Praedyth <centr@Praedyth-MacBook-Air.local>
2022-05-16 21:06:59 +0300

    fix

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Route, Switch } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 
 import Dashboard from '../pages/Dashboard'
 import Customers from '../pages/Customers'
@@ -10,6 +10,7 @@ const Routes = () => {
         <Switch>
             <Route path='/' exact component={Dashboard}/>
             <Route path='/customers' component={Customers}/>
+            <Redirect to='/' />
         </Switch>
     )
 }
